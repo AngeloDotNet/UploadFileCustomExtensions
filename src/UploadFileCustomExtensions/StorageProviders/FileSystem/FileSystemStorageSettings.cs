697e2f6e272dfb0ffa84c4b8dc5b0c665fe77464
Angelo Pirola <angelo9871@gmail.com>
2023-03-13 00:45:43 +0100

    Aggiunti Storage Providers

---
+++ src/UploadFileCustomExtensions/StorageProviders/FileSystem/FileSystemStorageSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace UploadFileCustomExtensions.StorageProviders.FileSystem;
+
+public class FileSystemStorageSettings
+{
+    public string StorageFolder { get; set; }
+}
---
+++ src/UploadFileCustomExtensions/StorageProviders/Storage/StorageProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UploadFileCustomExtensions.StorageProviders.FileSystem;
+
+namespace UploadFileCustomExtensions.StorageProviders.Storage;
+
+public static class StorageProviderExtensions
+{
+    public static IServiceCollection AddFileSystemStorageProvider(this IServiceCollection services, Action<FileSystemStorageSettings> configuration)
+    {
+        var settings = new FileSystemStorageSettings();
+        configuration.Invoke(settings);
+        services.AddSingleton(settings);
+
+        services.AddScoped<IStorageProvider, FileSystemStorageProvider>();
+        return services;
+    }
+}
