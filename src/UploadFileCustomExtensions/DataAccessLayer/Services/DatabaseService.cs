73f927d9a117ac0b7787237255ebea3a0b6cf0e9
Angelo Pirola <angelo9871@gmail.com>
2023-03-13 00:45:14 +0100

    Aggiunto Data Access Layer

---
+++ src/UploadFileCustomExtensions/DataAccessLayer/Services/DatabaseService.cs
@@ -0,0 +1,41 @@
+﻿using EFCoreGeneric.Infrastructure.Interfaces;
+using UploadFileCustomExtensions.DataAccessLayer.Entities;
+
+namespace UploadFileCustomExtensions.DataAccessLayer.Services;
+
+public class DatabaseService : IDatabaseService
+{
+    private readonly IUnitOfWork<ImageEntity, Guid> unitOfWork;
+
+    public DatabaseService(IUnitOfWork<ImageEntity, Guid> unitOfWork)
+    {
+        this.unitOfWork = unitOfWork;
+    }
+
+    public async Task<List<ImageEntity>> GetListItemAsync()
+    {
+        var listItem = await unitOfWork.ReadOnly.GetAllAsync();
+        return listItem;
+    }
+
+    public async Task<ImageEntity> GetItemAsync(Guid id)
+    {
+        var item = await unitOfWork.ReadOnly.GetByIdAsync(id);
+        return item;
+    }
+
+    public async Task CreateItemAsync(ImageEntity item)
+    {
+        await unitOfWork.Command.CreateAsync(item);
+    }
+
+    public async Task UpdateItemAsync(ImageEntity item)
+    {
+        await unitOfWork.Command.UpdateAsync(item);
+    }
+
+    public async Task DeleteItemAsync(ImageEntity item)
+    {
+        await unitOfWork.Command.DeleteAsync(item);
+    }
+}
---
+++ src/UploadFileCustomExtensions/DataAccessLayer/Services/IDatabaseService.cs
@@ -0,0 +1,12 @@
+﻿using UploadFileCustomExtensions.DataAccessLayer.Entities;
+
+namespace UploadFileCustomExtensions.DataAccessLayer.Services;
+
+public interface IDatabaseService
+{
+    Task<List<ImageEntity>> GetListItemAsync();
+    Task<ImageEntity> GetItemAsync(Guid id);
+    Task CreateItemAsync(ImageEntity item);
+    Task UpdateItemAsync(ImageEntity item);
+    Task DeleteItemAsync(ImageEntity item);
+}
