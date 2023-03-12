21901eff86eeb5c8c01934f115f2a0f43569bc78
Angelo Pirola <angelo9871@gmail.com>
2023-03-13 00:45:57 +0100

    Aggiunti Shared Models

---
+++ src/UploadFileCustomExtensions/Shared/Models/ImageRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using UploadFileCustomExtensions.Filters;
+
+namespace UploadFileCustomExtensions.Shared.Models;
+
+public class ImageRequest
+{
+    [BindRequired]
+    [AllowedExtensions("jpeg", "*.jpg", "*.png")]
+    public IFormFile File { get; set; }
+
+    public string Description { get; set; }
+}
---
+++ src/UploadFileCustomExtensions/Shared/Models/ImageResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace UploadFileCustomExtensions.Shared.Models;
+
+public class ImageResponse
+{
+    public Guid Id { get; set; }
+    public string Path { get; set; }
+    public int Length { get; set; }
+    public string ContentType { get; set; }
+    public string Description { get; set; }
+}
