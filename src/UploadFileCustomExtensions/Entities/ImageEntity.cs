0e1381242487f117bf6c1f8107ccd6dc5350b4bd
Angelo Pirola <angelo9871@gmail.com>
2023-03-12 23:33:44 +0100

    Primo commit

---
+++ src/UploadFileCustomExtensions/Entities/ImageEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace UploadFileCustomExtensions.Entities;
+
+public class ImageEntity
+{
+    public Guid Id { get; set; }
+    public string Path { get; set; }
+    public int Length { get; set; }
+    public string Description { get; set; }
+}
---
+++ src/UploadFileCustomExtensions/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using UploadFileCustomExtensions.Entities;
+global using UploadFileCustomExtensions.Models.Record;
+global using UploadFileCustomExtensions.Models.Request;
+global using UploadFileCustomExtensions.StorageProviders;
